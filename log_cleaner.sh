a676b9c7c7c4da1e2926bc5dd0dd560a05c9d840
Anis Emad <anisemad123@gmail.com>
2026-02-01 22:04:32 +0200

    made the log dir portable instead of hardcoded"

--- log_cleaner.sh
+++ log_cleaner.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 #using this path if you will schudle it using cron 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-LOG_DIR=$HOME/devopsroadmap/module2/ #change this to your path 
+LOG_DIR="${1:-./}" #change this to your path 
 ARCHIVE_DIR="$LOG_DIR/archive/"
 
 #making a dir for archive if it isn't there 
@@ -15,24 +15,20 @@ mkdir -p "$ARCHIVE_DIR"
 find "$LOG_DIR" -maxdepth 1 -name "*.log" -print0 | while IFS= read -r -d '' file; do 
     
     #checks if it's a regular file
-    [[ -f $file ]] || continue 
+    [[ -f "$file" ]] || continue 
 
     #calcuating the age of the file 
-    creation_time=$(stat -c %Y $file)
+    creation_time=$(stat -c %Y "$file")
     time_now=$(date +%s)
     differnce=$(( (time_now - creation_time) / 86400 ))
 
     # if it's 30 days it will be deleted if it's 3 or more will be compressed
     if (( differnce >= 30 )); then
         echo "we are deleting this file '$file'"
-        rm $file
+        rm "$file"
     elif (( differnce >= 3 )); then
         echo "we are compressing this file $file"
-        gzip $file
-        #checks if there is a dir with this name 
-        if [[ ! -d "$ARCHIVE_DIR" ]]; then
-            mkdir archive
-        fi
+        gzip "$file"
         #moving it to the archive dir 
         mv "${file}.gz" "$ARCHIVE_DIR"
     fi
